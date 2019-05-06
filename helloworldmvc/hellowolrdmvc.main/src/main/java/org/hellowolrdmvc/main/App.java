459141ffd1f59a1de6607f99150a6fcf2add9b7c
cedri <cedri@10.176.128.107>
2019-05-06 08:55:05 +0200

    phase 2

---
+++ helloworldmvc/hellowolrdmvc.main/src/main/java/org/hellowolrdmvc/main/App.java
@@ -0,0 +1,13 @@
+package org.hellowolrdmvc.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
